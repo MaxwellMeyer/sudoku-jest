39b0ce75a38194c68cd227f82187b57845bcde57
Jon Stump <jmstump@gmail.com>
2021-02-02 14:52:27 -0800

    add good fail for checkInteger
    Co-authored-by: Michael Burton <micbur1@gmail.com>
    Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -7,4 +7,8 @@ describe('Sudoku', () => {
     expect(sudoku.column).toEqual([2,5,7]);
     expect(sudoku.row).toEqual([1,2,3]);
   });
+  test('should determine that the entry is an integer between 1-9', () => {
+    const notANumber = new Sudoku([2,5,22],[1,2,3]);
+    expect(notANumber.checkInteger()).toEqual("one number is outside 1-9");
+  });
 });
--- src/sudoku.js
+++ src/sudoku.js
@@ -2,3 +2,11 @@ export default function Sudoku(column,row) {
   this.column = column;
   this.row = row;
 }
+
+Sudoku.prototype.checkInteger = function() {
+  // const input = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  // for (i = 0; i <= array.length; i ++)
+  //   if (!(array[i]).includes(input[i])) {
+  //     return NaN;
+  //   }
+}
